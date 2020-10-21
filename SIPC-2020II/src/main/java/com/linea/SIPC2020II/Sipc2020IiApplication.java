bba0eb5c1015814274a04f08f6f3ea68f7d1e091
AValeriaCM <anvalecasmo@outlook.es>
2020-10-21 12:41:01 -0500

    Commit inicial

---
+++ SIPC-2020II/src/main/java/com/linea/SIPC2020II/Sipc2020IiApplication.java
@@ -0,0 +1,13 @@
+package com.linea.SIPC2020II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sipc2020IiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sipc2020IiApplication.class, args);
+	}
+
+}
---
+++ SIPC-2020II/src/test/java/com/linea/SIPC2020II/Sipc2020IiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linea.SIPC2020II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sipc2020IiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
